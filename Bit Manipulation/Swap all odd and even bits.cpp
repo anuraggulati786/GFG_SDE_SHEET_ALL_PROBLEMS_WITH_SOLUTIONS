8402d686757451138bbcabb2c3fa06801040db36
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2022-04-08 20:39:41 +0530

    Add files via upload

---
+++ Bit Manipulation/Swap all odd and even bits.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to swap odd and even bits.
+    unsigned int swapBits(unsigned int n)
+    {
+    	// Your code here
+    	unsigned int res = 0;
+		for (int i = 0; i <= 30; i+=2) {
+			int a = n & (1 << i);
+			int b = n & (1 << (i + 1));
+			res |= (a << 1) | (b >> 1);
+   		 }
+		return res;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		unsigned int n;
+		cin>>n;//input n
+		
+		Solution ob;
+		//calling swapBits() method
+		cout << ob.swapBits(n) << endl;
+	}
+	return 0;
+}  // } Driver Code Ends
