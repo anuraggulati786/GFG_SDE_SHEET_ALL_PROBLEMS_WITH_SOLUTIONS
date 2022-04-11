9b4fc0572f118734104564dd9f4ddc3ea68bf8c8
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2022-04-11 13:31:00 +0530

    Create problems.cpp

---
+++ Arrays/problems.cpp
@@ -0,0 +1,17 @@
+Leaders in an array	
+Equilibrium point	
+Sort an array of 0s, 1s and 2s	
+Reverse array in groups	
+Convert array into Zig-Zag fashion	
+Rearrange array alternatively
+Missing number in array	
+K-th element of two sorted Arrays	
+Check if two arrays are equal or not	
+Kadane’s Algorithm
+Subarray with given sum	
+Trapping Rain Water	
+Minimum Platforms	
+Stock buy and sell	
+Largest Number formed from an Array	
+Largest subarray with 0 sum
+Swapping pairs make sum equal
