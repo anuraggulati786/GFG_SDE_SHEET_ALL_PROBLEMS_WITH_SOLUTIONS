d87fa17b0e56479ec267434dc3c626936392d34c
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2022-04-11 15:58:55 +0530

    Add files via upload

---
+++ Arrays/Sort an array of 0s, 1s and 2s.cpp
@@ -0,0 +1,72 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        // coode here 
+       int lo = 0;
+    int hi = n - 1;
+    int mid = 0;
+
+    // Iterate till all the elements
+    // are sorted
+    while (mid <= hi) {
+        switch (a[mid]) {
+
+        // If the element is 0
+        case 0:
+            swap(a[lo++], a[mid++]);
+            break;
+
+        // If the element is 1 .
+        case 1:
+            mid++;
+            break;
+
+        // If the element is 2
+        case 2:
+            swap(a[mid], a[hi--]);
+            break;
+        }
+    }
+   
+    }
+    
+};
+
+// { Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        Solution ob;
+        ob.sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
