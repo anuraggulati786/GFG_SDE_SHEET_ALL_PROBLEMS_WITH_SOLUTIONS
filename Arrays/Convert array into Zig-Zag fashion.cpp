269d34cf6f2ae47bf9e4006ac25eb29207d86208
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2022-04-11 20:45:10 +0530

    Add files via upload

---
+++ Arrays/Convert array into Zig-Zag fashion.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Program for zig-zag conversion of array
+	void zigZag(int arr[], int n) {
+	    // code here
+	    // simple pattern hai bhai ....
+	    // time complexity is O(N)...
+	    // space complexity is O(1)....
+	    int i=0,j=1;
+	    while(j<n){
+	        if(arr[i]>arr[j]){
+	            swap(arr[i],arr[j]);
+	        }
+	        i++;j++;
+	        if(j<n && arr[i] < arr[j]){
+	            swap(arr[i],arr[j]);
+	        }
+	        i++;j++;
+	    }
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.zigZag(arr, n);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
